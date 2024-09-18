c6bdceae1ff1edb16b3ef039ffb903e589552091
Squirm257 <benrc257@gmail.com>
2024-09-18 14:47:13 -0500

    added main function

--- Main/driver.cpp
+++ Main/driver.cpp
@@ -1,8 +1,14 @@
 /*
     Main function
-    Authors: Alec Szczechowicz, Benjamin Clark, 
+    Authors: Alec Szczechowicz, Benjamin Clark, Silas ---, Lucas Starkey
     Date: 9/12/2024
     Description: This program simulates a sushibar with a conveyor belt that sends random dishes with sushi out to the customer.
 */
 
 #include "restaurant.h"
+
+int main() {
+
+
+    return 0;
+}
